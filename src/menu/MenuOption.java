57481d81f7daaebc87e9104eb24c80049e6b2bad
Alexander Kuhn <alexander_kuhn@hotmail.de>
2014-05-26 09:25:40 +0200

    testvorführung

--- src/menu/MenuOption.java
+++ src/menu/MenuOption.java
@@ -11,7 +11,7 @@ public class MenuOption implements Screen {
 		Gdx.gl.glClearColor(0, 0, 0, 1); 		//black screen
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
-
+//dhjdfhALK
 	@Override
 	public void resize(int width, int height) {
 
